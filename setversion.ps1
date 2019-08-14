48f2bccab4fca27411e2662418baf2ddf58e96d2
Gareth Luckett <gareth.luckett@gmail.com>
2019-08-14 16:12:17 +0100

    Updated version file location

--- setversion.ps1
+++ setversion.ps1
@@ -1,7 +1,7 @@
-"package cli
+"package version
 var (
   PlatformName = """"
   Version      = ""$env:DOCKER_VERSION""
   GitCommit    = ""$(git rev-parse --short HEAD)""
   BuildTime    = ""$(get-date)""
-)" | Out-File -Encoding Ascii "C:\gopath\src\github.com\docker\cli\cli\version.go"
+)" | Out-File -Encoding Ascii "C:\gopath\src\github.com\docker\cli\cli\version\version.go"
